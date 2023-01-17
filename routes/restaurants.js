f4f6eab5eb931e092417a5eab96ba890e7396d6c
Palaniappan R <palaniappan.r.mail@gmail.com>
2023-01-17 09:43:51 +0530

    Added Comments and Fixed Route Errors

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -5,29 +5,30 @@ const { urlencoded } = require('express');
 const methodOverride = require('method-override');
 module.exports = router; 
 
+let temp
+
 router.use(urlencoded({ extended: true }));
 router.use(methodOverride('_method'));
 router.get('/' , (req , res) => {
     res.redirect('/restaurants');
 });
 
-router.get('/restaurants' , async (req, res) => {
+router.get('/restaurants' , async (req, res) => { //GET Route for Index Page
     const rests = await Restaurant.find({});
     res.render('index' , {rests});
 })
 
-router.get('/restaurants/new' , async(req , res) => {
+router.get('/restaurants/new' , async(req , res) => { //GET Route for new restaurant form
     res.render('new');
 })
 
-router.get('/restaurants/:id' , async(req , res) => {
+router.get('/restaurants/:id' , async(req , res) => { //GET Route to display info about a specific restaurant
     const {id} = req.params;
     const rest = await Restaurant.findById(id);
     res.render('show',{rest});
 })
 
-router.post('/restaurants/:id' , async(req , res) => {
-    console.log(req.body)
+router.post('/restaurants/:id' , async(req , res) => { //POST Route for new item form
     const {id} = req.params;
     const rest = await Restaurant.findById(id)
    rest.items.push(req.body)
@@ -43,7 +44,7 @@ router.post('/restaurants/:id' , async(req , res) => {
     res.redirect(`/restaurants/${id}`) 
 })
 
-router.get('/restaurants/newItem/:id' , async(req , res) => {
+router.get('/restaurants/newItem/:id' , async(req , res) => { //GET Route to add an item to a restaurant
     const {id} = req.params;
     const rest = await Restaurant.findById(id);
     rest.itemCount += 1
@@ -51,7 +52,7 @@ router.get('/restaurants/newItem/:id' , async(req , res) => {
     res.render('new_item',{id,rest});
 })
 
-router.post('/restaurants' , async(req , res) => {
+router.post('/restaurants' , async(req , res) => { //POST Route to add a Restaurant
     const rest = new Restaurant(req.body);
     if(req.body.cus1 == "on")
         rest.cuisines.push('Indian')
@@ -68,41 +69,53 @@ router.post('/restaurants' , async(req , res) => {
    rest.itemCount = 0
     rest.avgPrice = 0
     await rest.save();
-    console.log(rest)
     res.redirect('/restaurants')
 })
 
-router.get('/restaurants/:id/update' , async(req , res) => {
+router.get('/restaurants/:id/update' , async(req , res) => { //GET Route for edit restaurant form
     const {id} = req.params;
     const rest = await Restaurant.findById(id);
+    temp = [rest.items , rest.avgPrice , rest.itemCount]
     res.render('update',{rest});
 })
 
-router.put('/restaurants/:id' , async(req , res) => {
+router.put('/restaurants/:id' , async(req , res) => { //PUT Route to update restaurant details
     await Restaurant.findByIdAndUpdate(req.params.id , req.body)
+    const rest = await Restaurant.findById(req.params.id)
+    rest.items = temp[0]
+    rest.avgPrice = temp[1]
+    rest.itemCount = temp[2]
+    rest.save()
     res.redirect(`/restaurants/${req.params.id}`)
 })
 
-router.get('/restaurants/:rest_id/:item_id/delete' , async(req , res) => {
+router.delete('/restaurants/:rest_id/:item_id' , async(req , res) => { //DELETE Route to remove an item
     const rest_id = req.params.rest_id
     const item_id = req.params.item_id
     const rest = await Restaurant.findById(rest_id)
 
     for(i of rest.items){
-        if(JSON.stringify(i._id) == JSON.stringify(item_id))
+        if(JSON.stringify(i._id) == JSON.stringify(item_id)){
             i.remove()
+            rest.itemCount -= 1
+        }
     }
 
-    let avgP = 0
-    for(i of rest.items)
-        avgP += i.price
-    rest.avgPrice = (avgP/rest.itemCount)
+    if(rest.itemCount != 0){
+        let avgP = 0
+        for(i of rest.items)
+            avgP += i.price
+        rest.avgPrice = (avgP/rest.itemCount)
+    }
+    else
+        rest.avgPrice = 0
+
 
     rest.save()
     res.redirect(`/restaurants/${rest_id}`)
 })
 
-router.delete('/restaurants/:id' , async(req , res) => {
+router.delete('/restaurants/:id' , async(req , res) => { //DELETE Route to remove a restaurant
     await Restaurant.findByIdAndDelete(req.params.id , req.body)
     res.redirect(`/restaurants`)
 })
